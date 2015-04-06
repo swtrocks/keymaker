cbb1659adda11d45810f33a99a56eca6264f6e16
Steven Tsiang <coder.swt@gmail.com>
2015-04-05 20:12:49 -0700

    Update to 0.2.0
    Add boto dependency to requirements.txt

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('requirements.txt') as f:
 setup(
     name='keymaker',
     description="A tool for managing SSH keys",
-    version='0.1.0',
+    version='0.2.0',
     long_description=__doc__,
     packages=find_packages(),
     include_package_data=True,
