4a865778a58266fc8efd141d8b119be109f46d9d
Steven Tsiang <coder.swt@gmail.com>
2015-04-08 16:07:26 -0700

    Auto discovery of current instance metadata
    - Auto discovery of current instance metadata
    - Overwrite keys instead of appending
    - Remove group parameter prompt
    - Add basic logging

--- keymaker/__main__.py
+++ keymaker/__main__.py
@@ -5,15 +5,19 @@ import time
 import asyncio
 import boto
 import boto.utils
+import logging
 
 from datetime import datetime
 from daemon import runner
 
+LOG = logging.getLogger(__name__)
+logging.basicConfig(filename='/var/log/keymaker/keymakerlog.log', level=logging.ERROR)
+
 
 class Keymaker():
     def __init__(self):
-        self.group = None
         self.iam_role = None
+        self.group_name = None
         self.group_list = None
         self.pubkey_list = None
 
@@ -23,11 +27,23 @@ class Keymaker():
         iam_role = [role for role in inst_metadata['iam']['security-credentials']][0]
         return iam_role
 
+    def get_group_name(self):
+        inst_metadata = boto.utils.get_instance_metadata()
+        inst_id = inst_metadata['instance-id']
+        # retrieve tags based on instance-id
+        conn = boto.connect_ec2()
+        tags = conn.get_all_tags(filters={'resource-id': inst_id, 'key': 'Group'})
+        if tags:
+            group_name = tags[0].value
+            return group_name
+        else:
+            LOG.error('No "Group" tag for instance-id: {}'.format(inst_id))
+
     # get the list of users from the iam group
     def get_users_list(self):
         group_list = []
         conn = boto.connect_iam()
-        g = conn.get_group(self.group)
+        g = conn.get_group(self.group_name)
         for user in g.users:
             group_list.append(user.user_name)
         return group_list
@@ -54,7 +70,7 @@ class Keymaker():
 
     def add_keys(self):
         try:
-            with open('/home/scopely/.ssh/authorized_keys', 'a') as f:
+            with open('/home/scopely/.ssh/authorized_keys', 'w') as f:
                 for key in self.pubkey_list:
                     f.write(key + "\n\n")
                     os.chmod('/home/scopely/.ssh/authorized_keys', 0o600)
@@ -64,14 +80,14 @@ class Keymaker():
 
 
 @asyncio.coroutine
-def run_keymaker(group):
+def run_keymaker():
     while True:
         keymaker = Keymaker()
-        keymaker.group=group
 
         # find out which iam group should have access to this box
         # can listen on a queue
         keymaker.iam_role = keymaker.get_iam_role()
+        keymaker.group_name = keymaker.get_group_name()
 
         # get the emails from the iam group
         # first check if group exists
@@ -83,17 +99,22 @@ def run_keymaker(group):
         res_paths = keymaker.create_paths()
         res_add = keymaker.add_keys()
 
+        print('iam role: {}'.format(keymaker.iam_role))
+        print('group name: {}'.format(keymaker.group_name))
+        print('group list: {}'.format(keymaker.group_list))
+        print('pubkey list: {}'.format(keymaker.pubkey_list))
+
         time.sleep(5)
     pass
 
 
 @click.command()
-@click.argument('group')
-def main(group):
+#@click.argument('group')
+def main():
     loop = asyncio.get_event_loop()
 
     try:
-        loop.run_until_complete(run_keymaker(group))
+        loop.run_until_complete(run_keymaker())
     finally:
         loop.close()
 
